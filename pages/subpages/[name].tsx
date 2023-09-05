b674b4b44bb32f02d1333dbb074cd812c0b8fa66
박준형 <108733795+krapli441@users.noreply.github.com>
2023-09-05 17:28:17 +0900

    작업 종료 전 주석 변경

--- pages/subpages/[name].tsx
+++ pages/subpages/[name].tsx
@@ -1,8 +1,7 @@
 import fetch from "isomorphic-unfetch";
 
 interface User {
-  name: string; // 예상되는 타입에 맞게 수정
-  // 다른 속성들도 필요한 경우 여기에 추가
+  name: string;
 }
 
 const Users = ({ user }: { user: User }) => {
