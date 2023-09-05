28b9abe702ac08c4c585e863b82e209b6e1d9e7b
박준형 <108733795+krapli441@users.noreply.github.com>
2023-09-05 16:03:07 +0900

    동적으로 라우팅

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ export default function Home() {
         <h1>hello next.js</h1>
         <Link href="/about">move to index page</Link>
         <Link href="/projects">move to about page</Link>
+        <Link href="/subpages/blahblah">move to info page</Link>
       </div>
     </main>
   );
---
+++ pages/subpages/[info].tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Info() {
+  return (
+    <div>
+      <h1>Info</h1>
+      <h2>as you know, i'm practice next.js.</h2>
+      <Link href="/">move to main page</Link>
+    </div>
+  );
+}
