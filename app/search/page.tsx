c6b9c108b15689103ceab929ec8b2f13f57ff6b9
박준형 <108733795+krapli441@users.noreply.github.com>
2023-09-06 16:58:22 +0900

    in progress

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,12 +1,15 @@
 import Link from "next/link";
 
-
 const Search = () => {
   return (
     <>
-      <div>
+      <div className="flex flex-col justify-center items-center h-screen">
         <p>search 페이지</p>
-        <Link href={"/"}>메인으로 돌아가기</Link>
+        <Link href={"/"}>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            메인으로 돌아가기
+          </button>
+        </Link>
       </div>
     </>
   );
--- app/services/page.tsx
+++ app/services/page.tsx
@@ -3,9 +3,13 @@ import Link from "next/link";
 const Services = () => {
   return (
     <>
-      <div>
+      <div className="flex flex-col justify-center items-center h-screen">
         <p>services 페이지</p>
-        <Link href={"/"}>메인으로 돌아가기</Link>
+        <Link href={"/"}>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            메인으로 돌아가기
+          </button>
+        </Link>
       </div>
     </>
   );
