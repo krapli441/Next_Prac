57c2fc24159b10396e69c83eeb976bb47baa3f0b
박준형 <108733795+krapli441@users.noreply.github.com>
2023-09-06 16:56:12 +0900

    tailwind 요리조리

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <div>
         <p className="text-2xl">imnotkrapli</p>
       </div>
-      <div>
+      <div className="flex gap-3">
         <Link href="/search">
           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
             github 검색
