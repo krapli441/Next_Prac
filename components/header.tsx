b306811f9e4d88a751548fccc1267e4c24e24829
박준형 <108733795+krapli441@users.noreply.github.com>
2023-09-07 15:30:09 +0900

    차크라 UI가 좋아보인다.

--- components/header.tsx
+++ components/header.tsx
@@ -6,7 +6,7 @@ function Header() {
   return (
     <header className="flex w-full h-32 text-center items-center header-text">
       <div className="w-2/6">
-        <p className="text-5xl font-mono">IMNOTKRAPLI</p>
+        <p className="text-5xl font-mono">SOMETHING SHIT</p>
       </div>
       <div className="w-4/6">
         <ul className="list-none flex gap-20 items-center mt-4 text-2xl">
